56d32f0cb386fe10659acdc72d7db303a4a31982
Pedro Marques <ppmarq1@gmail.com>
2022-10-01 09:02:45 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = Message.create([
+    {greeting:"Hello Rails!"},
+    {greeting:"Hello JavaScript!"},
+    {greeting:"Hello Ruby!"},
+    {greeting:"Hello React!"},
+    {greeting:"Hello Redux!"}
+  ])
+  
+  puts 'greetings created...'
